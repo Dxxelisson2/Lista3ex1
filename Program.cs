35d48294bd94a6520fdceaacdfb32a45ae6ba97c
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-05-30 15:29:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+
+            {
+                int valor;
+
+                do
+                {
+                    Console.WriteLine("Digite um valor positivo!");
+                    valor = int.Parse(Console.ReadLine());
+
+                    if (valor < 0)
+                    {
+                        Console.WriteLine("Valor inválido! Digite novamente.");
+                    }
+                    else if (valor > 0)
+                    {
+                        Console.WriteLine("Você digitou um valor positivo. Parabéns!");
+                    }
+                } while (valor <= 0);
+
+            }
+        }
+
+    }
+}
+
